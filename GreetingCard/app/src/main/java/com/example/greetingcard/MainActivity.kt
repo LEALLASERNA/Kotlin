76aa993cf3d7932e0d0638a32fe6298e5075de42
ALEXLEALdev <alejandrolealdev@gmail.com>
2024-11-03 14:22:24 +0100

    LEAL

--- GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello my name ols $name!",
+        text = "Hello my name oleal $name!",
         modifier = modifier
     )
 }
